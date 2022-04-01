c700dc5ccf19c7441c3a7cd50f07613e53a833b8
David Martinez Bosch <davmarbos@alu.edu.gva.es>
2022-04-01 13:01:18 +0200

    subida inicial del proyecto peepoclown

---
+++ GestionVentasV3/src/main/java/com/loren/gestionventasv3/POJO/Comercial.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.loren.gestionventasv3.POJO;
+
+/**
+ *
+ * @author DAW
+ */
+public class Comercial {
+
+    private int id;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private float comision;
+
+    public Comercial() {
+        super();
+
+    }
+
+    public Comercial(int id, String nombre, String apellido1, String apellido2, float comision) {
+        super();
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.comision = comision;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public float getComision() {
+        return comision;
+    }
+
+    public void setComision(float comision) {
+        this.comision = comision;
+    }
+
+    public String toString() {
+        return "Comercial{" + "id=" + id + ", nombre=" + nombre + ", apellido1=" + apellido1 + ", apellido2=" + apellido2 + ", comision=" + comision + '}';
+    }
+
+    
+}
